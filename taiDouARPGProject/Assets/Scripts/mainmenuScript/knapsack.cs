2c3e31528dd597afecd12fe8e0cfee8bbb8794be
ZhanSongTao <312526310@qq.com>
2016-07-30 21:57:06 +0800

    drug & Box 物品的信息查看

--- taiDouARPGProject/Assets/Scripts/mainmenuScript/knapsack.cs
+++ taiDouARPGProject/Assets/Scripts/mainmenuScript/knapsack.cs
@@ -4,16 +4,21 @@ using System.Collections;
 public class knapsack : MonoBehaviour 
 {
     private EquipPopup equipPopup;
+    private InventoryPopup inventoryPopup;
 
     void Awake()
     {
         equipPopup = transform.Find("EquipPopup").GetComponent<EquipPopup>();
+        inventoryPopup = transform.Find("InventoryPopup").GetComponent<InventoryPopup>();
     }
     public void OnInventoryClick(object[] objectArray)
     {
         InventoryItem it = objectArray[0] as InventoryItem;
         bool isLeft = (bool)objectArray[1];
 
-        equipPopup.Show(it, isLeft);
+        if (it.Inventory.InventoryTYPE == InventoryType.Equip)
+            equipPopup.Show(it, isLeft);
+        else
+            inventoryPopup.Show(it);
     }
 }
